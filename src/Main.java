faa3fc3140288ea8c252c6d8ef317c966fb54a7f
GutoAS <agostinhosoberano2@gmail.com>
2022-12-21 10:57:18 +0200

    fizzbuzz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
+
+
+        if ((number % 3 == 0) && (number % 5 ==0))
+            System.out.println("FizzBuzz");
+        else if (number % 3 == 0)
+            System.out.println("Buzz");
+        else  if (number % 5 == 0)
+            System.out.println("Fizz");
+        else
+            System.out.println(number);
+
     }
 }
